8f0169baf650966359f2234a3086aa266815498b
arunaShakthi <arunashakthi002@gmail.com>
2026-07-13 12:35:18 +0530

    feat: initialize project structure with sqlc, database models, and postgres vendor dependencies

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/arunaShakthi/web-scraper/internal/db"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *db.Queries
+}
 
 func main() {
 	fmt.Println("Hello World")
@@ -19,6 +28,20 @@ func main() {
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT environment variable is not set")
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL environment variable is not set")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Error connecting to database: ", err)
+	}
+
+	apiCfg := apiConfig{
+		DB: db.New(conn),
 	}
 
 	router := chi.NewRouter()
@@ -35,6 +58,7 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerError)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
@@ -44,7 +68,7 @@ func main() {
 	}
 
 	log.Printf("Server is running on port %s", portString)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
---
+++ models.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"time"
+
+	"github.com/arunaShakthi/web-scraper/internal/db"
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string    `json:"name"`
+}
+
+func databaseUserToUser(dbUser db.User) User {
+	return User{
+		ID:        dbUser.ID,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+		Name:      dbUser.Name,
+	}
+}
