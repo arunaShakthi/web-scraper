a0eafe974a6bf4e739e2eec4f5c5458ee9cfa89c
arunaShakthi <arunashakthi002@gmail.com>
2026-07-13 12:38:35 +0530

    refactor: update User model and helper function to use internal db package and correct struct syntax

--- models.go
+++ models.go
@@ -1,17 +1,24 @@
 package main
 
-type user struct {
+import (
+	"time"
+
+	"github.com/arunaShakthi/web-scraper/internal/db"
+	"github.com/google/uuid"
+)
+
+type User struct {
 	ID        uuid.UUID `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	Name      string    `json:"name"`
 }
 
-func databaseUserToUser(dbUser database.User) User {
-	return User (
-		ID: dbUser.ID,
+func databaseUserToUser(dbUser db.User) User {
+	return User{
+		ID:        dbUser.ID,
 		CreatedAt: dbUser.CreatedAt,
 		UpdatedAt: dbUser.UpdatedAt,
-		Name: dbUser.Name,
-	)
+		Name:      dbUser.Name,
+	}
 }
