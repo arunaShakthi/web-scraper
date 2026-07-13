2dee99b7b12e16f1a3cc436210ebf5e07f4ed06c
arunaShakthi <arunashakthi002@gmail.com>
2026-07-13 12:35:33 +0530

    fix: correct typos in package name and refactor User struct mapping syntax

--- models.go
+++ models.go
@@ -1,24 +1,17 @@
-package main
+packege main
 
-import (
-	"time"
-
-	"github.com/arunaShakthi/web-scraper/internal/db"
-	"github.com/google/uuid"
-)
-
-type User struct {
+type user struct {
 	ID        uuid.UUID `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	Name      string    `json:"name"`
 }
 
-func databaseUserToUser(dbUser db.User) User {
-	return User{
-		ID:        dbUser.ID,
+func databaseUserToUser(dbUser database.User) User {
+	return User (
+		ID: dbUser.ID,
 		CreatedAt: dbUser.CreatedAt,
 		UpdatedAt: dbUser.UpdatedAt,
-		Name:      dbUser.Name,
-	}
+		Name: dbUser.Name,
+	)
 }
